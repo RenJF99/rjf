a5c5795326465fcf8ad09b66382a1c8f46631646
15947382750@sina.cn <15947382750@sina.cn>
2021-03-20 10:51:39 +0800

    提交代码

--- cheer/cheers.c
+++ cheer/cheers.c
@@ -4,10 +4,15 @@
 #include <string.h>
 int main()
 {
-	int ch = getchar();
+	int ch = 0;
+	while ((ch = getchar) != EOF)
+	{
+		putchar(ch);
+	}
+	/*int ch = getchar();
 
 	putchar(ch);
-	printf("%c\n", ch);
+	printf("%c\n", ch);*/
 	return 0;
 }
 
